780730e61e6c093bc702b264810eb5af57ac4439
richardhan <hxy.314@gmail.com>
2023-10-07 22:39:01 +1100

    update freamework

--- todo-react-typescript-vite/src/main.tsx
+++ todo-react-typescript-vite/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './app/App'
 
-const App = () => {return <>This is app</>}
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />);
  
